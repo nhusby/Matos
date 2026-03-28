97d8802404d98037c2862d960e7eb6139808b8f3
nhusby <nhusby@gmail.com>
2026-03-27 20:20:21 -0700

    init

---
+++ src/lib/Agent.ts
@@ -0,0 +1,236 @@
+
+import OpenAI from 'openai';
+import { EmitterPromise } from "./EmitterPromise";
+
+export interface Tool {
+  name: string;
+  description: string;
+  params?: any;
+  callback: (params: any) => Promise<string>;
+}
+export interface ToolCall {
+  id: string;
+  name: string;
+  params: any;
+  result?: Promise<string>;
+}
+
+export interface ToolPart {
+  role: 'tool';
+  name: string;
+  content: string;
+  toolCallId: string;
+}
+export interface AgentPart {
+  role: 'assistant';
+  content: string;
+  toolCalls?: (ToolCall & { _argStr?: string })[];
+}
+export interface Message {
+  role: 'user' | 'assistant' | 'system' | 'tool';
+  name?: string;
+  content: string;
+  parts?: (AgentPart | ToolPart)[];
+  thinking?: boolean;
+  loading?: boolean;
+  created: Date;
+}
+
+interface Api extends OpenAI {
+  _models: string[];
+}
+
+
+
+export abstract class Agent {
+  protected apis!: Api[];
+  public get api(): Api {
+    // TODO: add external state to cycle
+    return this.apis[0];
+  }
+
+  protected models!: string[];
+  public get model(): string {
+    for (const model of this.models) {
+      if (this.api._models.includes(model)) {
+        return model;
+      }
+    }
+    return this.models[0];
+  }
+
+  messages: Message[] = [];
+
+  tools: Tool[] = [];
+
+  constructor(params: { api: Api | Api[]; model: string | string[] }) {
+    this.apis = Array.isArray(params.api) ? params.api : [params.api];
+    this.models = Array.isArray(params.model) ? params.model : [params.model];
+  }
+
+  async init() {
+    for (const api of this.apis) {
+      api._models = [];
+      for await (const page of (await api.models.list()).iterPages()) {
+        for (const model of page.getPaginatedItems()) {
+          api._models.push(model.id);
+        }
+      }
+    }
+
+    return this;
+  }
+
+  public sendMessage(message: Message): EmitterPromise<Message> {
+    this.messages.push(message);
+    const response: Message = {
+      role: 'assistant',
+      content: '',
+      parts: [],
+      created: new Date(),
+    };
+    this.messages.push(response);
+
+    return this.sendMessages(this.messages, response);
+  }
+
+  public sendMessages(messages: Message[], response: Message): EmitterPromise<Message> {
+    const emitter = new EmitterPromise<Message>();
+    const part: AgentPart = {
+      role: 'assistant',
+      content: '',
+      toolCalls: [],
+    };
+    response.parts!.push(part);
+
+    (async () => {
+      const abortController = new AbortController();
+      const stream = await this.api.chat.completions.create({
+        stream: true,
+        signal: abortController.signal,
+        model: this.model,
+        messages: toOpenAiMessages(messages),
+      } as OpenAI.ChatCompletionCreateParamsStreaming);
+      for await (const chunk of stream) {
+        try {
+          for (const choice of chunk.choices) {
+            if (choice.delta.content) {
+              part.content += choice.delta.content;
+              response.content += choice.delta.content;
+              await emitter.emitAsync('chunk', choice.delta.content);
+            } else if (choice.delta.refusal) {
+              part.content += choice.delta.refusal;
+              response.content += choice.delta.refusal;
+              await emitter.emitAsync('chunk', choice.delta.refusal);
+            }
+            if (choice.delta.tool_calls) {
+              for (const toolCall of choice.delta.tool_calls) {
+                if (!part.toolCalls![toolCall.index]) {
+                  // MaybeDo emit/invoke ToolCalls as they come in?
+                  part.toolCalls![toolCall.index] = {
+                    id: toolCall.id!,
+                    name: '',
+                    params: {},
+                    _argStr: '',
+                  };
+                }
+
+                if (toolCall.function?.name) {
+                  part.toolCalls![toolCall.index]!.name =
+                    (part.toolCalls![toolCall.index]!.name || '') + toolCall.function.name;
+                }
+                if (toolCall.function?.arguments) {
+                  part.toolCalls![toolCall.index]!._argStr += toolCall.function.arguments;
+                }
+              }
+            }
+
+            if (choice.finish_reason) {
+              if (choice.finish_reason === 'tool_calls') {
+                for (const toolCall of part.toolCalls!) {
+                  const toolCallResult: ToolPart = {
+                    role: 'tool',
+                    name: toolCall.name,
+                    content: '',
+                    toolCallId: toolCall.id,
+                  };
+                  response.parts!.push(toolCallResult);
+                  try {
+                    if (toolCall._argStr) {
+                      toolCall.params = JSON.parse(toolCall._argStr);
+                    }
+                    await emitter.emitAsync('toolCall', {
+                      ...toolCall,
+                      result: undefined,
+                      argStr: undefined,
+                    });
+                    const tool = this.tools.find((tool) => tool.name === toolCall.name);
+                    if (!tool) {
+                      throw new Error(`tool ${toolCall.name} not found`);
+                    }
+                    toolCall.result = tool.callback(toolCall.params);
+                    toolCallResult.content = await toolCall.result;
+                  } catch (e: any) {
+                    toolCall.result = Promise.resolve(e.message);
+                  }
+                  await emitter.emitAsync('toolCallResult', toolCallResult);
+                }
+                await this.sendMessages(this.messages, response).onAny((event, data) =>
+                  emitter.emitAsync(event, data),
+                );
+              } else {
+                await emitter.emitAsync('finished', response);
+              }
+              break;
+            }
+          }
+        } catch (error: any) {
+          emitter.emit('error', error);
+          abortController.abort();
+          throw error;
+        }
+      }
+
+      return response;
+    })().then(
+      (response) => {
+        emitter.resolve(response!);
+      },
+      (e) => emitter.reject(e),
+    );
+
+    return emitter;
+  }
+}
+
+function snake_case(key: string) {
+  return key.replace(/([A-Z])/g, '_$1').toLowerCase();
+}
+
+function toOpenAiMessages(messages: Message[]) {
+  return messages
+    .flatMap((msg) => (msg.parts ?? [msg]) as any)
+    .map((part: AgentPart | ToolPart | Message) => {
+      const obj: any = {};
+      for (const key of ['role', 'name', 'content', 'toolCalls', 'toolCallId']) {
+        if (key in part) {
+          // @ts-ignore
+          obj[snake_case(key)] = part[key];
+        }
+      }
+      if (obj.tool_calls) {
+        obj.tool_calls = obj.tool_calls.map((toolCall: ToolCall) => {
+          return {
+            id: toolCall.id,
+            type: 'function',
+            function: {
+              name: toolCall.name,
+              arguments: JSON.stringify(toolCall.params),
+            },
+          };
+        });
+      }
+
+      return obj as any;
+    });
+}
---
+++ src/lib/EmitterPromise.ts
@@ -0,0 +1,42 @@
+import { EventEmitter2 } from 'eventemitter2';
+
+export class EmitterPromise<T> extends EventEmitter2 implements Promise<T> {
+    private resolveFn?: (value: T) => void;
+    private rejectFn?: (error: Error) => void;
+    private promise = new Promise<T>((resolve, reject) => {
+        this.resolveFn = resolve;
+        this.rejectFn = reject;
+    });
+
+    readonly [Symbol.toStringTag] = 'Promise';
+
+    then<TResult1 = T, TResult2 = never>(
+        resolve?: ((value: T) => TResult1 | PromiseLike<TResult1>) | null,
+        reject?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null,
+    ) {
+        const promise = this.promise.then(resolve, reject) as Promise<T>;
+
+        return Object.setPrototypeOf({ promise }, this) as Promise<TResult1 | TResult2> &
+            EmitterPromise<T>;
+    }
+
+    catch<TResult = never>(reject?: ((reason: unknown) => TResult | PromiseLike<TResult>) | null) {
+        this.promise.catch(reject);
+
+        return this;
+    }
+
+    finally(fn?: () => void) {
+        this.promise.finally(fn);
+
+        return this;
+    }
+
+    resolve(value: T) {
+        this.resolveFn?.(value);
+    }
+
+    reject(error: Error) {
+        this.rejectFn?.(error);
+    }
+}
